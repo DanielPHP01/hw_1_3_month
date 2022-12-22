f84616673f44fbfc275ac32bc5a1228e80cc0f53
DanielPHP01 <danielmokushov01@gmail.com>
2022-12-22 15:51:09 +0600

    update fix

--- app/src/main/java/com/example/hw_1_3_month/MainActivity.java
+++ app/src/main/java/com/example/hw_1_3_month/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.hw_1_3_month;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
@@ -14,31 +12,52 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    EditText editText1;
+    EditText email;
+    Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-            EditText editText = findViewById(R.id.edit_text_one);
-            EditText editText1 = findViewById(R.id.edit_text_two);
-            EditText email = findViewById(R.id.editTextTextEmailAddress);
-            Button button = findViewById(R.id.button);
+        inisView();
+        setClickListener();
+    }
+
+    private void setClickListener() {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_SEND);
-                intent.putExtra(Intent.EXTRA_EMAIL, new String[]{email.getText().toString()});
-                intent.putExtra(Intent.EXTRA_SUBJECT, editText.getText().toString());
-                intent.putExtra(Intent.EXTRA_TEXT, editText1.getText().toString());
-                intent.setType("message/rfc822");
-                startActivity(Intent.createChooser(intent, "dfsdfsdfw"));
-                Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    v.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
-                } else {
-                    v.vibrate(100000);
-                }
+                startVibration();
+                startIntent();
             }
         });
+    }
+
+    private void startVibration() {
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            v.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+            v.vibrate(100000);
+        }
+    }
+
+    private void startIntent() {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{email.getText().toString()});
+        intent.putExtra(Intent.EXTRA_SUBJECT, editText.getText().toString());
+        intent.putExtra(Intent.EXTRA_TEXT, editText1.getText().toString());
+        intent.setType("message/rfc822");
+        startActivity(Intent.createChooser(intent, "dfsdfsdfw"));
+    }
+
+    private void inisView() {
+        email = findViewById(R.id.editTextTextEmailAddress);
+        editText1 = findViewById(R.id.edit_text_two);
+        editText = findViewById(R.id.edit_text_one);
+        button = findViewById(R.id.button);
     }
 }
